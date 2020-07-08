6d5293fae2c950d9217274c205d6dc74f03ba516
pjgowtham <pjgowtham@gmail.com>
2020-07-08 15:33:40 +0530

    Add missing oppo_checks header entry

--- drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
+++ drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
@@ -40,6 +40,7 @@
 #include "touchpanel_common.h"
 #include "touchpanel_healthinfo.h"
 #include "util_interface/touch_interfaces.h"
+#include <linux/oppo_checks.h>
 
 #if GESTURE_RATE_MODE
 #include "gesture_recon_rate.h"
