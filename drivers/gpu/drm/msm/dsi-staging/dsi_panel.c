ceb21c15f8baec1a9402873283f70235b362d726
negrroo <mohammedaelnaggar1@gmail.com>
2020-07-30 04:36:35 +0200

    Kernel: Sched: add some new Cpu govs
    Signed-off-by: negrroo <mohammedaelnaggar1@gmail.com>
    Signed-off-by: Cykeek <Cykeek@proton.me>
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -20,6 +20,11 @@
 #include <video/mipi_display.h>
 #include <linux/firmware.h>
 
+#ifdef VENDOR_EDIT //Cong.Dai@BSP.TP.Function, 2019/07/03, modified for replace daily build macro
+#include <soc/oppo/oppo_project.h>
+#endif /* VENDOR_EDIT */
+
+#include <linux/display_state.h>
 #include "dsi_panel.h"
 #include "dsi_ctrl_hw.h"
 
@@ -62,6 +67,13 @@ enum dsi_dsc_ratio_type {
 	DSC_12BPC_8BPP,
 	DSC_RATIO_TYPE_MAX
 };
+
+bool display_on = true;
+
+bool is_display_on(void)
+{
+	return display_on;
+}
 
 static u32 dsi_dsc_rc_buf_thresh[] = {0x0e, 0x1c, 0x2a, 0x38, 0x46, 0x54,
 		0x62, 0x69, 0x70, 0x77, 0x79, 0x7b, 0x7d, 0x7e};
