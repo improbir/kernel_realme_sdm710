fef3330735fc45cb715e1d1372c417631f8506ce
CloudedQuartz <ravenklawasd@gmail.com>
2021-02-05 10:08:05 +0530

    cpu-boost: skip boost when battery saver is on
    cpu-boost is disabled, but keep this here so I don't lose it.
    
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -24,6 +24,9 @@
 #include <linux/time.h>
 #include <linux/battery_saver.h>
 
+// Battery saver
+#include <linux/battery_saver.h>
+
 struct cpu_sync {
 	int cpu;
 	unsigned int input_boost_min;
@@ -193,6 +196,10 @@ static void do_input_boost(struct work_struct *work)
 	unsigned int i, ret;
 	struct cpu_sync *i_sync_info;
 
+	if (is_battery_saver_on()) {
+		pr_info("Skipping boost as battery saver is on\n");
+		return;
+	}
 	cancel_delayed_work_sync(&input_boost_rem);
 	if (sched_boost_active) {
 		sched_set_boost(0);
