585f291e43b33171edc1ad19438cfa6f802416f2
Cykeek <Cykeek@proton.me>
2023-07-05 15:16:20 +0530

    drivers: power: Fix -Wstrict-prototypes
    Fixes:
    ../drivers/power/oppo/oppo_vooc.c:67:30: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
    void oppo_vooc_battery_update()
                                 ^
                                  void
    ../drivers/power/oppo/oppo_vooc.c:1113:41: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
    bool oppo_vooc_get_fastchg_low_temp_full()
                                            ^
                                             void
    2 errors generated.
    make[4]: *** [../scripts/Makefile.build:342: drivers/power/oppo/oppo_vooc.o] Error 1
    
    Signed-off-by: Cykeek <Cykeek@proton.me>
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- drivers/power/oplus/oplus_vooc.c
+++ drivers/power/oplus/oplus_vooc.c
@@ -66,7 +66,7 @@ int __attribute__((weak)) register_devinfo(char *name, struct manufacture_info *
 }
 static int oplus_vooc_convert_fast_chg_type(int fast_chg_type);
 
-void oplus_vooc_battery_update()
+void oplus_vooc_battery_update(void)
 {
 	struct oplus_vooc_chip *chip = g_vooc_chip;
 /*
@@ -1476,7 +1476,7 @@ void oplus_vooc_set_fastchg_to_warm_false(void)
 	}
 }
 
-bool oplus_vooc_get_fastchg_low_temp_full()
+bool oplus_vooc_get_fastchg_low_temp_full(void)
 {
 	if (!g_vooc_chip) {
 		return false;
